f1ed5e7a483857c4f126058b9ce0f25bc1cb1329
Ronak Makadiya <makadiya@amazon.com>
2022-03-07 13:23:18 -0500

    code change for main account CFNs

--- scripts/environment-deploy.sh
+++ scripts/environment-deploy.sh
@@ -25,6 +25,8 @@ function componentDeploy {
   COMPONENT_NAME=$2
 
   pushd "$SOLUTION_DIR/$COMPONENT_DIR" > /dev/null
+  printf '\n ..... Printting path ....\n'
+  printf $PWD
   printf "\nDeploying component: %s ...\n\n" "$COMPONENT_NAME"
   $EXEC sls deploy -s "$STAGE"
   printf "\nDeployed component: %s successfully \n\n" "$COMPONENT_NAME"
@@ -42,9 +44,102 @@ function goComponentDeploy() {
   popd > /dev/null
 }
 
+function upload_docker_image_ecr() {
+
+  pushd "$SOLUTION_DIR/pre-deployment" > /dev/null
+  local stack_name_pre_deployment
+  stack_name_pre_deployment=$($EXEC sls info -s "$STAGE" | grep 'stack:' --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')
+  popd > /dev/null
+
+  aws_region="$(cat "$CONFIG_DIR/settings/$STAGE.yml" "$CONFIG_DIR/settings/.defaults.yml" 2> /dev/null | grep '^awsRegion:' -m 1 --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')"
+  ECR_Repository_Name="$(aws cloudformation describe-stacks --stack-name "$stack_name_pre_deployment" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`ECRRepositoryName`].OutputValue')"
+  aws_account_number="$(aws cloudformation describe-stacks --stack-name "$stack_name_pre_deployment" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`AWSAccountNumber`].OutputValue')"
+ 
+  # Deploy UI image to ECR
+  pushd "$SOLUTION_DIR/ui" > /dev/null
+
+  # first we package locally (to populate .env.local only)
+  printf "\nPackaging website UI\n\n"
+  $EXEC sls package-ui --local=true -s "$STAGE"
+  # then we package for deployment
+  # (to populate .env.production and create a build via "npm build")
+  $EXEC sls package-ui -s "$STAGE"
+  
+  # docker commands
+  aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $aws_account_number.dkr.ecr.$aws_region.amazonaws.com
+  docker build -t $ECR_Repository_Name .
+  docker tag $ECR_Repository_Name:latest $aws_account_number.dkr.ecr.$aws_region.amazonaws.com/$ECR_Repository_Name:latest
+  docker push $aws_account_number.dkr.ecr.$aws_region.amazonaws.com/$ECR_Repository_Name:latest
+
+  popd > /dev/null
+
+}
+
+function upload_docker_image_ecr_without_build() {
+
+  pushd "$SOLUTION_DIR/pre-deployment" > /dev/null
+  local stack_name_pre_deployment
+  stack_name_pre_deployment=$($EXEC sls info -s "$STAGE" | grep 'stack:' --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')
+  popd > /dev/null
+
+  aws_region="$(cat "$CONFIG_DIR/settings/$STAGE.yml" "$CONFIG_DIR/settings/.defaults.yml" 2> /dev/null | grep '^awsRegion:' -m 1 --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')"
+  ECR_Repository_Name="$(aws cloudformation describe-stacks --stack-name "$stack_name_pre_deployment" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`ECRRepositoryName`].OutputValue')"
+  aws_account_number="$(aws cloudformation describe-stacks --stack-name "$stack_name_pre_deployment" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`AWSAccountNumber`].OutputValue')"
+ 
+  pushd "$SOLUTION_DIR/ui" > /dev/null
+
+  npm run build
+ 
+  aws ecr get-login-password --region $aws_region | docker login --username AWS --password-stdin $aws_account_number.dkr.ecr.$aws_region.amazonaws.com
+  docker build -t $ECR_Repository_Name .
+  docker tag $ECR_Repository_Name:latest $aws_account_number.dkr.ecr.$aws_region.amazonaws.com/$ECR_Repository_Name:latest
+  docker push $aws_account_number.dkr.ecr.$aws_region.amazonaws.com/$ECR_Repository_Name:latest
+
+  popd > /dev/null
+
+}
+
+function update_lambda_proxy_env_variable() {
+
+  pushd "$SOLUTION_DIR/backend" > /dev/null
+  local stack_name_backend
+  stack_name_backend=$($EXEC sls info -s "$STAGE" | grep 'stack:' --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')
+  popd > /dev/null
+
+  pushd "$SOLUTION_DIR/infrastructure" > /dev/null
+  local stack_name_infrastructure
+  stack_name_infrastructure=$($EXEC sls info -s "$STAGE" | grep 'stack:' --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')
+  popd > /dev/null
+
+  aws_region="$(cat "$CONFIG_DIR/settings/$STAGE.yml" "$CONFIG_DIR/settings/.defaults.yml" 2> /dev/null | grep '^awsRegion:' -m 1 --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')"
+  api_endpoint="$(aws cloudformation describe-stacks --stack-name "$stack_name_backend" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`ServiceEndpoint`].OutputValue')"
+
+  lambda_proxy_arn="$(aws cloudformation describe-stacks --stack-name "$stack_name_infrastructure" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`LambdaProxyArn`].OutputValue')"
+  aws lambda update-function-configuration --function-name $lambda_proxy_arn --environment Variables={APIGW_URL=$api_endpoint}
+
+}
+
+function force_new_esc_deployment() {
+
+  pushd "$SOLUTION_DIR/infrastructure" > /dev/null
+  local stack_name_infrastructure
+  stack_name_infrastructure=$($EXEC sls info -s "$STAGE" | grep 'stack:' --ignore-case | sed 's/ //g' | cut -d':' -f2 | tr -d '\012\015')
+  popd > /dev/null
+
+  ClusterName="$(aws cloudformation describe-stacks --stack-name "$stack_name_infrastructure" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`ClusterName`].OutputValue')"
+  ServiceName="$(aws cloudformation describe-stacks --stack-name "$stack_name_infrastructure" --output text --region "$aws_region" --query 'Stacks[0].Outputs[?OutputKey==`ServiceName`].OutputValue')"
+  
+  aws ecs update-service --cluster $ClusterName --service $ServiceName --force-new-deployment
+}
+
+componentDeploy "pre-deployment" "Pre-Deployment"
+
+#Upload the dockder image without build so that 'infrastructure' get reference.
+upload_docker_image_ecr_without_build
+
 disableStats "infrastructure"
 componentDeploy "infrastructure" "Infrastructure"
-componentDeploy "pre-deployment" "Pre-Deployment"
+
 
 # We now need to invoke the pre deployment lambda (we can do this locally)
 #$EXEC sls invoke local -f preDeployment -s $STAGE
@@ -54,6 +149,12 @@ $EXEC sls invoke -f preDeployment -s "$STAGE"
 popd > /dev/null
 
 componentDeploy "backend" "Backend"
+
+# update the Lambda Proxy environment variable to get APIGW URL 
+update_lambda_proxy_env_variable
+# update complete
+
+
 componentDeploy "edge-lambda" "Edge-Lambda"
 componentDeploy "post-deployment" "Post-Deployment"
 goComponentDeploy "environment-tools" "Environment-Tools"
@@ -65,21 +166,13 @@ pushd "$SOLUTION_DIR/post-deployment" > /dev/null
 $EXEC sls invoke -f postDeployment -l -s "$STAGE"
 popd > /dev/null
 
-# Deploy UI
-pushd "$SOLUTION_DIR/ui" > /dev/null
 
-# first we package locally (to populate .env.local only)
-printf "\nPackaging website UI\n\n"
-$EXEC sls package-ui --local=true -s "$STAGE"
-# then we package for deployment
-# (to populate .env.production and create a build via "npm build")
-$EXEC sls package-ui -s "$STAGE"
+# uploading docker image again with build version.
+upload_docker_image_ecr
 
-printf "\nDeploying website UI\n\n"
-# Deploy it to S3, invalidate CloudFront cache
-$EXEC sls deploy-ui --invalidate-cache=true -s "$STAGE"
-printf "\nDeployed website UI successfully\n\n"
-popd > /dev/null
+
+#Force new ECS service deployment so that it uses new uploaded image
+force_new_esc_deployment
 
 printf "\n----- ENVIRONMENT DEPLOYED SUCCESSFULLY 🎉 -----\n\n"
 pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
